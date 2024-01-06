a5697d33afb0f5e657900eee63c244e0362438bc
Santo Shakil <shakil.mezbah@gmail.com>
2024-01-06 14:00:03 +0600

    naming fix

--- src/lib.rs
+++ src/lib.rs
@@ -6,37 +6,37 @@ use log4rs::{
 };
 
 #[no_mangle]
-pub extern "C" fn log_e(message: *const libc::c_char) {
+pub extern "C" fn log_e_ffi(message: *const libc::c_char) {
     let message = unsafe { std::ffi::CStr::from_ptr(message) };
     let message = message.to_str().unwrap().to_string();
     log::error!("{}", message);
 }
 
 #[no_mangle]
-pub extern "C" fn log_w(message: *const libc::c_char) {
+pub extern "C" fn log_w_ffi(message: *const libc::c_char) {
     let message = unsafe { std::ffi::CStr::from_ptr(message) };
     let message = message.to_str().unwrap().to_string();
     log::warn!("{}", message);
 }
 
 #[no_mangle]
-pub extern "C" fn log_i(message: *const libc::c_char) {
+pub extern "C" fn log_i_ffi(message: *const libc::c_char) {
     let message = unsafe { std::ffi::CStr::from_ptr(message) };
     let message = message.to_str().unwrap().to_string();
     log::info!("{}", message);
 }
 
 #[no_mangle]
-pub extern "C" fn init_logrs(path: *const libc::c_char) -> libc::c_int {
+pub extern "C" fn init_logrs_ffi(path: *const libc::c_char) -> libc::c_int {
     let path = unsafe { std::ffi::CStr::from_ptr(path) };
     let path = path.to_str().unwrap().to_string();
-    match init_logrs_r(path) {
+    match init_logrs(path) {
         Ok(_) => 0,
         _ => 1,
     }
 }
 
-fn init_logrs_r(path: String) -> Result<(), std::io::Error> {
+pub fn init_logrs(path: String) -> Result<(), std::io::Error> {
     let p = PatternEncoder::new("{d(%H:%M:%S)} - {l} - {m}\n");
     let stderr = ConsoleAppender::builder().encoder(Box::new(p)).build();
 
@@ -74,13 +74,13 @@ mod tests {
     fn test_logrs() {
         let path = std::ffi::CString::new("./logs.log").unwrap();
         let path = path.as_ptr();
-        assert_eq!(init_logrs(path), 0);
+        assert_eq!(init_logrs_ffi(path), 0);
 
         let message = std::ffi::CString::new("test").unwrap();
         let message = message.as_ptr();
 
-        assert_eq!(log_i(message), ());
-        assert_eq!(log_w(message), ());
-        assert_eq!(log_e(message), ());
+        assert_eq!(log_i_ffi(message), ());
+        assert_eq!(log_w_ffi(message), ());
+        assert_eq!(log_e_ffi(message), ());
     }
 }
